e370f8013e0b5ef1d01f5421c79b46be1bcf1619
abdulaziz <aboudecharkawi@gmail.com>
2024-01-23 16:58:30 +0200

    pagination useQuery

--- Monla-LLC/src/Pages/Products/Products.jsx
+++ Monla-LLC/src/Pages/Products/Products.jsx
@@ -5,19 +5,43 @@ import ProductNav from "../../Components/ProductNav/ProductNav"
 import { useFetchData } from "../../CustomHook/GetData"
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { useQuery } from "react-query"
+import axios from "axios"
+
 
 
 const Products = () => {
+
   
-  const api=`${import.meta.env.VITE_REACT_APP_BACKEND}/product/read/all`
-  const {data ,error, loading}=useFetchData(api)
+  // const api=`${import.meta.env.VITE_REACT_APP_BACKEND}/product/read/all`
+  // const {data ,error, loading}=useFetchData(api)
   const [selectedCategory, setSelectedCategory]=useState([])
+  const [currentPage, setCurrentPage]=useState(1)
 
   const categoryApi=`${import.meta.env.VITE_REACT_APP_BACKEND}/category/readCategory`
   const {data:categoryData, loading:loadingCategory, error: errorCategory }=useFetchData(categoryApi)
 
-  const filteredProduct= data.filter(product=>
-    selectedCategory.includes(product.category))
+  const {
+    isPending: isProductPending,
+    error: productError,
+    data:productData
+  }=useQuery({
+    queryKey:["productData", currentPage],
+    queryFn:async()=>{
+      try {
+        const response= await axios.get(`${import.meta.env.VITE_REACT_APP_BACKEND}/product/read/all?pageNumber=${currentPage}&pageSize=${4}`)
+        console.log(response.data)
+        return response.data
+      } catch (error) {
+        
+      }
+    }
+  })
+
+  // const filteredProduct= data.filter(product=>
+  //   selectedCategory.includes(product.category))
 
 
     const handleCategoryChange = category => {
@@ -45,15 +69,24 @@ const Products = () => {
       />
       </div>
       <div className={Styles.product}>
-      {loading && <p>Loading...</p>}
-          {error && <p>Error: {error.message}</p>}
-        {data.map((item)=>(
-          <Link to={`/productdetails/${item.slug}`} state={item}>
-          <ProductCart key={item.id} price={item.price} img={`${import.meta.env.VITE_REACT_APP_BACKEND}/${item.image}`} title={item.title} desc={item.description} />
-          </Link>
+      {isProductPending && <p>Loading...</p>}
+          {productError && <p>Error: {productError.message}</p>}
+        {productData && productData.map((item)=>(
+          <Link key={item.id} to={`/productdetails/${item.slug}`} state={item}>
+          <ProductCart key={item.id} item={item} img={`${import.meta.env.VITE_REACT_APP_BACKEND}/${item.image}`}  />
+           </Link>
         ))}
       </div>
       </div>
+      <Stack spacing={2}>
+        {productData && ( <Pagination
+              count={Math.ceil(productData)}
+              page={currentPage}
+              onChange={(event, page) => setCurrentPage(page)}
+              variant="outlined"
+           />)}
+     
+    </Stack>
     </div>
   )
 }
