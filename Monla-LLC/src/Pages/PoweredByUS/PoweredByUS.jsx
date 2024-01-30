649be99364f93cbb0ee91796e5a7fc5a8ec147a6
Rachwan <rachwan.harb2023@gmail.com>
2024-01-30 21:01:22 +0200

    create it a new page

---
+++ Monla-LLC/src/Pages/PoweredByUS/PoweredByUS.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+import styles from "./PoweredByUS.module.css";
+import { Link } from "react-router-dom";
+import rachwan from "../../assets/developers/rachwan-harb.jpg";
+
+const PoweredByUS = () => {
+  return (
+    <main className={`container ${styles.main}`}>
+      <div className={styles.wrapper}>
+        <div className={styles.imageContainer}>
+          <img src={rachwan} alt="Rachwan Harb" className={styles.image} />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Rachwan Harb</h1>
+          <div className={styles.emailWrapper}>
+            <Link to={"mailto:rachwan.harb2023@gmail.com"} className={styles.email}>
+              <span>rachwan.harb2023@gmail.com</span>
+            </Link>
+          </div>
+          <a
+            href="https://wa.me/76445648"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.number}>
+            +961 76 445 648
+          </a>
+          <div className={styles.links}>
+            <Link to={"https://www.linkedin.com/in/rachwan-harb"}>
+              <span>LinkedIn</span>
+            </Link>
+            <Link to={"https://github.com/Rachwan"}>
+              <span>GitHub</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.imageContainer}>
+          <img src={rachwan} alt="Rachwan Harb" className={styles.image} />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Rachwan Harb</h1>
+          <div className={styles.emailWrapper}>
+            <Link to={"mailto:rachwan.harb2023@gmail.com"} className={styles.email}>
+              <span>rachwan.harb2023@gmail.com</span>
+            </Link>
+          </div>
+          <a
+            href="https://wa.me/76445648"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.number}>
+            +961 76 445 648
+          </a>
+          <div className={styles.links}>
+            <Link to={"https://www.linkedin.com/in/rachwan-harb"}>
+              <span>LinkedIn</span>
+            </Link>
+            <Link to={"https://github.com/Rachwan"}>
+              <span>GitHub</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.imageContainer}>
+          <img src={rachwan} alt="Rachwan Harb" className={styles.image} />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Rachwan Harb</h1>
+          <div className={styles.emailWrapper}>
+            <Link to={"mailto:rachwan.harb2023@gmail.com"} className={styles.email}>
+              <span>rachwan.harb2023@gmail.com</span>
+            </Link>
+          </div>
+          <a
+            href="https://wa.me/76445648"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.number}>
+            +961 76 445 648
+          </a>
+          <div className={styles.links}>
+            <Link to={"https://www.linkedin.com/in/rachwan-harb"}>
+              <span>LinkedIn</span>
+            </Link>
+            <Link to={"https://github.com/Rachwan"}>
+              <span>GitHub</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.imageContainer}>
+          <img src={rachwan} alt="Rachwan Harb" className={styles.image} />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Rachwan Harb</h1>
+          <div className={styles.emailWrapper}>
+            <Link to={"mailto:rachwan.harb2023@gmail.com"} className={styles.email}>
+              <span>rachwan.harb2023@gmail.com</span>
+            </Link>
+          </div>
+          <a
+            href="https://wa.me/76445648"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.number}>
+            +961 76 445 648
+          </a>
+          <div className={styles.links}>
+            <Link to={"https://www.linkedin.com/in/rachwan-harb"}>
+              <span>LinkedIn</span>
+            </Link>
+            <Link to={"https://github.com/Rachwan"}>
+              <span>GitHub</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.imageContainer}>
+          <img src={rachwan} alt="Rachwan Harb" className={styles.image} />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Rachwan Harb</h1>
+          <div className={styles.emailWrapper}>
+            <Link to={"mailto:rachwan.harb2023@gmail.com"} className={styles.email}>
+              <span>rachwan.harb2023@gmail.com</span>
+            </Link>
+          </div>
+          <a
+            href="https://wa.me/76445648"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.number}>
+            +961 76 445 648
+          </a>
+          <div className={styles.links}>
+            <Link to={"https://www.linkedin.com/in/rachwan-harb"}>
+              <span>LinkedIn</span>
+            </Link>
+            <Link to={"https://github.com/Rachwan"}>
+              <span>GitHub</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default PoweredByUS;
