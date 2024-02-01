08364da41c7e8f88da6cfef2b1cbbc7cec293c76
souhad <souhadmoussa86@gmail.com>
2024-02-01 10:46:56 +0200

    did google analytics

--- Monla-LLC/src/main.jsx
+++ Monla-LLC/src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import reportWebVitals from "./reportWebVitals.jsx";
 import { UserProvider } from './UserContext/UserContext.jsx'
 import { HelmetProvider } from 'react-helmet-async'
+import ReactGA from 'react-ga4'
+
+
+ReactGA.initialize("G-Y09EZVPTZ0");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
@@ -14,3 +19,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </HelmetProvider>
     </React.StrictMode>
 )
+
+
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
+reportWebVitals(SendAnalytics);
